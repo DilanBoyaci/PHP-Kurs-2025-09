9da3743249dac001341dea520b7fa7e0bab9ccae
Dilan <48790527+DilanBoyaci@users.noreply.github.com>
2025-09-22 13:50:19 +0200

    start

--- Filmverwaltung/030_Film_anzeigen/index.php
+++ Filmverwaltung/030_Film_anzeigen/index.php
@@ -10,29 +10,26 @@ $stmt = $db->query('SELECT * FROM filme;');
 $filme = $stmt->fetchAll();
 // var_dump($filme);
 
-// var_dump($filme[0]);  // array(9)
+var_dump($filme[0]);  // array(9)
 
 // echo $filme[0]['titel'];  // Equalizer
 
 $headings = array_keys($filme[0]);
 // Spaltenüberschriften entfernen:
-unset($headings[0]);  // In PHP werden Arrays nicht neu indiziert
+//unset($headings[0]);  // In PHP werden Arrays nicht neu indiziert
 unset($headings[8]);
-var_dump($headings);
-
+// var_dump($headings);
 
 foreach ($headings as $k => $v) {
     if ($v == 'fsk') $v = strtoupper($v);
     $headings[$k] = $v;
 }
-
-
 $headings = array_map('ucfirst', $headings);
 
 foreach ($filme as $key => $film) {
-unset($film['id']);     // Inhalte entfernen
-unset($film['cover']);  // Inhalte entfernen
-$filme[$key] = $film;
+    // unset($film['id']);     // Inhalte entfernen
+    unset($film['cover']);  // Inhalte entfernen
+    $filme[$key] = $film;
 }
 
 ?>
@@ -53,22 +50,24 @@ $filme[$key] = $film;
 
     <tr>
         <?php foreach ($headings as $heading) { ?>
-        <th>
-            <?php echo $heading; ?>
-        </th>
+            <th>
+                <?php echo $heading; ?>
+            </th>
         <?php } ?>
     </tr>
 
     <?php foreach ($filme as $film) { ?>
-    <tr>
-        <?php foreach ($film as $f) { ?>
 
-            <td>
-                <?php echo $f; ?>
-            </td>
+        <tr onclick="location.href='film_anzeigen.php?id=<?php echo $film['id']; ?>'">
+            <?php foreach ($film as $f) { ?>
 
-        <?php } ?>
-    </tr>
+                <td>
+                    <?php echo $f; ?>
+                </td>
+
+            <?php } ?>
+        </tr>
+
     <?php } ?>
 
 
